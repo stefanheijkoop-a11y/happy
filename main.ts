a83dc2745b900a5ca0dad0b585bc875b3b03c99a
stefanheijkoop-a11y <stefanheijkoop@gmail.com>
2026-05-30 08:55:28 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 1; index++) {
+    	
+    }
+})
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    # # # # #
+    `)
